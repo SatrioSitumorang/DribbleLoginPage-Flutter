7a97c0dccf690e97422343de69521b6ca0db887c
Satrio Holmes Afrido Situmorang <96107190+SatrioSitumorang@users.noreply.github.com>
2023-05-01 11:56:02 +0700

    Add files via upload

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+import 'MainPage.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: MainPage(),
+    );
+  }
+}
